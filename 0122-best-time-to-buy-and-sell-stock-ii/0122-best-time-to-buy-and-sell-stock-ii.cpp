bbdcc8df83001609ecfcd0bf49e526e87ae1febd
Mohamed Ahmed <mohammedahmmed4920@gmail.com>
2025-01-25 00:02:56 +0200

    Time: 0 ms (100%), Space: 17.1 MB (55.12%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,26 +1,15 @@
 class Solution {
-    int whileIncreasing(vector<int>& prices, int n, int index) {
-
-        index++;
-        while (index < n && prices[index] >= prices[index - 1]) {
-            index++;
-        }
-
-        return index - 1;
-    }
-
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
 
         int res = 0;
-        for (int i = 0; i < n; i++) {
-            int buy = prices[i];
-            int sellDay = whileIncreasing(prices, n, i);
-            res += (prices[sellDay] - buy);
-
-            i = sellDay;
+        for(int i = 0; i < n - 1;i++){
+            if(prices[i+1] > prices[i]){
+                res += prices[i+1] - prices[i];
+            }
         }
+        
         return res;
     }
 };
