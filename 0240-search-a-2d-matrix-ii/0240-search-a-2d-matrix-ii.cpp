dc7e1dd15dfbba9cdec83a910c8149796aa395ed
Mohamed Ahmed <mohammedahmmed4920@gmail.com>
2025-02-10 23:34:17 +0200

    Time: 47 ms (81.77%), Space: 18.6 MB (67.63%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    bool search(vector<vector<int>>& matrix, int target, int fr, int sr, int fc,
-                int sc) {
-        if (fr > sr || fc > sc) {
-            return false;
-        }
-
-        int midr = (fr + sr) / 2;
-        int midc = (fc + sc) / 2;
-
-        if (matrix[midr][midc] == target) {
-            return true;
-        }
-        if (matrix[midr][midc] > target) {
-            return search(matrix, target, fr, sr, fc, midc - 1) ||
-                   search(matrix, target, fr, midr - 1, fc, sc);
-        }
-        return search(matrix, target, fr, sr, midc + 1, sc) ||
-               search(matrix, target, midr + 1, sr, fc, sc);
-    }
-
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int fr = 0, sr = matrix.size() - 1;
-        int fc = 0, sc = matrix[0].size() - 1;
+        int m = matrix.size();
+        int row = 0, col =  matrix[0].size() - 1;
 
-        return search(matrix, target, fr, sr, fc, sc);
+        while(row < m && col >= 0){
+            if(target == matrix[row][col]){
+                return true;
+            }
+
+            if(target > matrix[row][col]){
+                row++;
+            }else{
+                col--;
+            }
+        }
+        return false;
     }
 };
