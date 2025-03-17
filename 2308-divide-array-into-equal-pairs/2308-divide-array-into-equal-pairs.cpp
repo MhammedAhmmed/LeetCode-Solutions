2c2c313934dca9f51989e00b31630fb786f8a02e
Mohamed Ahmed <mohammedahmmed4920@gmail.com>
2025-03-17 13:15:13 +0200

    Time: 7 ms (19.84%), Space: 18.1 MB (49.04%) - LeetHub

---
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+
+        unordered_map<int,int>freq;
+        
+        for(int num: nums){
+            freq[num]++;
+        }
+
+        for(auto it: freq){
+            if(it.second & 1){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
